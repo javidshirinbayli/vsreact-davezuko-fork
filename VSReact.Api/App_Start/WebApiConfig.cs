3dc17e59c0c1f99a7158466e12ef6d8e27b562bb
Mike H. DeVita <mhdevita@devita.co>
2016-03-28 20:16:49 -0700

    initial commit

---
+++ VSReact.Api/App_Start/WebApiConfig.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using VSReact.Api.App_Start;
+
+namespace VSReact.Api
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            var cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
+
+            var json = GlobalConfiguration.Configuration.Formatters.JsonFormatter;
+            json.SerializerSettings = new JsonSerializerSettings()
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                NullValueHandling = NullValueHandling.Ignore
+            };
+
+
+            // JSON only
+            config.Services.Replace(typeof(IContentNegotiator), new JsonContentNegotiator(json));
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ VSReact.Web/src/middleware/Api.js
@@ -0,0 +1,10 @@
+﻿import 'whatwg-fetch'
+
+export const Api = {
+  get: (endpoint, params) => {
+    return fetch(endpoint)
+    .then((res) => {
+      return res.json().then(json => ({ json, res }));  
+    })
+  }
+}
